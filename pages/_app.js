50c12045472b4d5b9b088b5f2b1c444cf90ef95a
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-10-23 19:57:46 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,41 @@
-import Layout from "@/components/Layout/Layout";
-import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
---
+++ utils/dbConnect.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { errorToJSON } from "next/dist/server/render";
+
+const DB_URL =
+  "mongodb+srv://divyagurav2608:vqdHkQhYGVQyMHEA@cluster0.wvskmzn.mongodb.net/?retryWrites=true&w=majority"
+
+if (!DB_URL) {
+  throw new Error(errorToJSON("DB ERROR"));
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(DB_URL, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default dbConnect;
