f4095dcc7508073163682c8bd371a21e4c129142
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-10-23 20:00:23 +0530

    Add files via upload

--- pages/api/task/[id].js
+++ pages/api/task/[id].js
@@ -25,14 +25,14 @@ const Handler = async (req, res) => {
     }
   }
 
-  // if (method === "DELETE") {
-  //   try {
-  //     await Task.findByIdAndDelete(id);
-  //     res.status(200).json({ message: "Task Deleted Successfully" });
-  //   } catch (error) {
-  //     res.status(500).json({ message: "Internal Server Error" });
-  //     console.log(error);
-  //   }
-  // }
+  if (method === "DELETE") {
+    try {
+      await Task.findByIdAndDelete(id);
+      res.status(200).json({ message: "Task Deleted Successfully" });
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+      console.log(error);
+    }
+  }
 };
 export default Handler;
